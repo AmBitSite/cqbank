6164892686f121c264c7dc6a5795330c0c342748
Gette <37911525+AmBitSite@users.noreply.github.com>
2019-09-10 16:36:29 +0300

    Add files via upload

--- mainPage/js/script.js
+++ mainPage/js/script.js
@@ -22,18 +22,24 @@ $(".nav-btn").click(function(){
         $(".nav").addClass("changeHeight")
         $(".nav-block").addClass("changeHeight")
         $(".nav-wrapp").fadeToggle()
+        $(".nav-btn__line:nth-child(1)").addClass("nav-btn__line-1st")
+        $(".nav-btn__line:nth-child(2)").addClass("nav-btn__line-2st")
+        $(".nav-btn__line:nth-child(3)").addClass("hide")
     }
     else if($(".nav").hasClass("changeHeight")){
         $(".nav").removeClass("changeHeight")
         $(".nav-block").removeClass("changeHeight")
         $(".nav-wrapp").fadeToggle()
+        $(".nav-btn__line:nth-child(1)").removeClass("nav-btn__line-1st")
+        $(".nav-btn__line:nth-child(2)").removeClass("nav-btn__line-2st")
+        $(".nav-btn__line:nth-child(3)").removeClass("hide")
     }  
 })
 
-let test = document.querySelector(".hide-menu__id");
-let parent = document.querySelector(".hide-menu-block")
-let test2 = document.querySelector(".hide-menu__pass")
-let mainParent = document.querySelector(".hide-menu")
+let test = document.querySelector("hide-menu__id");
+let parent = document.querySelector("hide-menu-block")
+let test2 = document.querySelector("hide-menu__pass")
+let mainParent = document.querySelector("hide-menu")
 mainParent.addEventListener("click", (e)=>{
     let x = e.target;
     if (x == test){
